d5acecac3f90c442995b4c825c85b328ab9a8a9d
inhueman <fabian.kinyua@gmail.com>
2022-09-08 13:12:54 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
